99b89236fc84254e8bd75da3f64232ddf171e3ae
growver-code <35625934+growver-code@users.noreply.github.com>
2021-06-01 08:45:45 +0900

    リファクタリングをAPPとInputTodoで実施

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
 import "./styles.css";
-// InputTodoからでimportを実行
+// importでそれぞれのファイルの定数を読み込み
 
 import { InputTodo } from "./components/InputTodo";
 import { IncompleteTodos } from "./components/IncompleteTodos";
 import { CompleteTodos } from "./components/CompleteTodos";
-// コンポーネント化より開始
-// 変数名をキャメルケースへ
+
 export const App = () => {
-  // todoテキスト内がはじめは空っぽ
-  const [todoText, setTodoText] = useState([""]);
-  // incompleteの部分にデータを反映
-  const [incompleteTodos, setnCompleteTodos] = useState([]);
+  // 各領域のデータをセット
+  const [todoText, setTodoText] = useState([]);
+  const [incompleteTodos, setinCompleteTodos] = useState([]);
   const [completeTodos, setCompleteTodos] = useState([]);
+
   // 文字を入力したら、値を取得する
   const onChangetodoText = (event) => setTodoText(event.target.value);
 
@@ -22,34 +21,39 @@ export const App = () => {
     if (todoText === "") return;
     // 入力した内容を新しく追加 配列形式で実施
     const newTodos = [...incompleteTodos, todoText];
-    setnCompleteTodos(newTodos);
+    setinCompleteTodos(newTodos);
     // 追加の部分にデータが残っているので、消去
     setTodoText("");
   };
 
   const onClickDelete = (index) => {
     const newTodos = [...incompleteTodos];
+    // newTodosに入れた配列のindexを取り除いてタスクから削除する
     newTodos.splice(index, 1);
-    setnCompleteTodos(newTodos);
+    setinCompleteTodos(newTodos);
   };
 
   const onClickComplete = (index) => {
     const newIncompleteTodos = [...incompleteTodos];
     newIncompleteTodos.splice(index, 1);
 
+    // 完了を押したらnewCompleteへ追加
+    // 未完成にあるimCompleteのデータを再編集
     const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
-    setnCompleteTodos(newIncompleteTodos);
+    setinCompleteTodos(newIncompleteTodos);
     setCompleteTodos(newCompleteTodos);
   };
 
+  // 戻るを押した場合の処理を実施
   const onClickBack = (index) => {
     const newCompleteTodos = [...completeTodos];
     newCompleteTodos.splice(index, 1);
 
     const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
     setCompleteTodos(newCompleteTodos);
-    setnCompleteTodos(newIncompleteTodos);
+    setinCompleteTodos(newIncompleteTodos);
   };
+  //ボタン操作の後、各ファイルにデータを送る
   return (
     <>
       <InputTodo
--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export const InputTodo = (props) => {
+  // props内の３つのデータを定数に格納
   const { todoText, onChange, onClick } = props;
   return (
     <div className="input-area">
