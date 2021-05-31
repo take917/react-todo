01acd88741774da30d354422ae4d0edc38ccfbe3
growver-code <35625934+growver-code@users.noreply.github.com>
2021-05-31 17:32:06 +0900

    コンポーネント化の実施

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import "./styles.css";
+// InputTodoからでimportを実行
 
+import { InputTodo } from "./components/InputTodo";
+import { IncompleteTodos } from "./components/IncompleteTodos";
 // コンポーネント化より開始
 // 変数名をキャメルケースへ
 export const App = () => {
   // todoテキスト内がはじめは空っぽ
   const [todoText, setTodoText] = useState([""]);
   // incompleteの部分にデータを反映
-  const [inCompleteTodos, setinCompleteTodos] = useState([]);
+  const [incompleteTodos, setnCompleteTodos] = useState([]);
   const [completeTodos, setCompleteTodos] = useState([]);
   // 文字を入力したら、値を取得する
   const onChangetodoText = (event) => setTodoText(event.target.value);
@@ -17,24 +20,24 @@ export const App = () => {
     // 空白で追加を押した場合、追加作業を行わない
     if (todoText === "") return;
     // 入力した内容を新しく追加 配列形式で実施
-    const newTodos = [...inCompleteTodos, todoText];
-    setinCompleteTodos(newTodos);
+    const newTodos = [...incompleteTodos, todoText];
+    setnCompleteTodos(newTodos);
     // 追加の部分にデータが残っているので、消去
     setTodoText("");
   };
 
   const onClickDelete = (index) => {
-    const newTodos = [...inCompleteTodos];
+    const newTodos = [...incompleteTodos];
     newTodos.splice(index, 1);
-    setinCompleteTodos(newTodos);
+    setnCompleteTodos(newTodos);
   };
 
   const onClickComplete = (index) => {
-    const newIncompleteTodos = [...inCompleteTodos];
+    const newIncompleteTodos = [...incompleteTodos];
     newIncompleteTodos.splice(index, 1);
 
-    const newCompleteTodos = [...completeTodos, inCompleteTodos[index]];
-    setinCompleteTodos(newIncompleteTodos);
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setnCompleteTodos(newIncompleteTodos);
     setCompleteTodos(newCompleteTodos);
   };
 
@@ -42,37 +45,23 @@ export const App = () => {
     const newCompleteTodos = [...completeTodos];
     newCompleteTodos.splice(index, 1);
 
-    const newIncompleteTodos = [...inCompleteTodos, completeTodos[index]];
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
     setCompleteTodos(newCompleteTodos);
-    setinCompleteTodos(newIncompleteTodos);
+    setnCompleteTodos(newIncompleteTodos);
   };
   return (
     <>
-      {/* classNameを当てることで、CSSのスタイルを適用している */}
-      <div className="input-area">
-        {/* onChangeを使うことで、クリックするとonClickaddが走るように設定 */}
-        <input
-          placeholder="TODOを入力"
-          value={todoText}
-          onChange={onChangetodoText}
-        />
-        <button onClick={onClickadd}>追加</button>
-      </div>
-      <div className="incomplete-area">
-        <p className="title">未完了のTODO</p>
-        <ul>
-          {/* mapを使う時、、バックで差分のみのレンダリングをしているため、keyを使って場所の設定が必要  */}
-          {inCompleteTodos.map((todo, index) => {
-            return (
-              <div key={todo} className="list-row">
-                <li>{todo}</li>
-                <button onClick={() => onClickComplete(index)}>完了</button>
-                <button onClick={() => onClickDelete(index)}>削除</button>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
+      <InputTodo
+        todoText={todoText}
+        onChange={onChangetodoText}
+        onClick={onClickadd}
+      />
+      <IncompleteTodos
+        todos={incompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
+
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
